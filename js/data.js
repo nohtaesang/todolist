7b1813cf99381834f7aa5bc58f5b3b2278ab0b81
nohtaesang <nohtaesang@naver.com>
2019-03-22 23:06:17 +0900

    First commit

---
+++ js/data.js
@@ -0,0 +1,17 @@
+let cards = [
+	{
+		checked: false,
+		content: '내용'
+	},
+	{
+		checked: false,
+		content: '내용2'
+	}
+];
+
+let drag = {
+	init_drag: false,
+	move_drag: false,
+	dX: 0,
+	dY: 0
+};
